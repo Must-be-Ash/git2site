6d04d41527914f55f30ae82ae497d34ab4ded499
Must-be-Ash <arash.nouruzi@yahoo.com>
2024-10-26 12:25:02 -0700

    Your commit message here

--- lib/db.ts
+++ lib/db.ts
@@ -6,33 +6,41 @@ if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
 }
 
-let cached = global.mongoose;
-
-if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null };
+interface CachedConnection {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
 }
 
-export async function connectDB() {
-  if (cached.conn) {
-    return cached.conn;
+declare global {
+  var mongooseConnection: CachedConnection;
+}
+
+if (!global.mongooseConnection) {
+  global.mongooseConnection = { conn: null, promise: null };
+}
+
+export async function connectDB(): Promise<typeof mongoose> {
+  if (global.mongooseConnection.conn) {
+    return global.mongooseConnection.conn;
   }
 
-  if (!cached.promise) {
+  if (!global.mongooseConnection.promise) {
     const opts = {
       bufferCommands: false,
     };
 
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose;
-    });
+    global.mongooseConnection.promise = mongoose.connect(MONGODB_URI!, opts)
+      .then((mongoose) => {
+        return mongoose;
+      });
   }
 
   try {
-    cached.conn = await cached.promise;
+    global.mongooseConnection.conn = await global.mongooseConnection.promise;
   } catch (e) {
-    cached.promise = null;
+    global.mongooseConnection.promise = null;
     throw e;
   }
 
-  return cached.conn;
+  return global.mongooseConnection.conn;
 }
